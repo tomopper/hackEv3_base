448a01d44702cd9de57495be80d7ed7dc78401d1
scvxl11344@yahoo.co.jp <scvxl11344@yahoo.co.jp>
2020-08-04 11:12:09 +0900

    TurnAngleを追加

---
+++ judge/TurnAngleJudge.cpp
@@ -0,0 +1,29 @@
+#include "TurnAngleJudge.h"
+
+TurnAngleJudge::TurnAngleJudge(){
+    float mStartAngle=0.0;
+    float mFinishAngle=0.0;
+
+
+
+}
+
+void TurnAngleJudge::setStartAngle(){
+
+
+
+}
+
+
+void TurnAngleJudge::setFinishAngle(float finangle){
+
+
+
+}
+
+
+bool TurnAngleJudge:: run(){
+
+
+
+}
