38cb06eb562c7327c0a58a21acc24f842c62f308
scvxl11344@yahoo.co.jp <scvxl11344@yahoo.co.jp>
2020-08-25 17:36:15 +0900

    修正

--- section/SpeedSectionManager.cpp
+++ section/SpeedSectionManager.cpp
@@ -22,182 +22,7 @@ SpeedSectionManager::SpeedSectionManager() : SectionManager()
     addSection(sc);
   }
 
-  /*  Section* sc = new Section();
-
-    VirtualLineTracer2* Virtual2 = (VirtualLineTracer2*)sc->selectWalker(Section::VIRTUAL2);
   
-    Virtual2->setAbsTurnAngle(-20);
-    Virtual2->setParam(10,5, 0.2, 0.2,1,1);
-    LengthJudge* Length = (LengthJudge*)sc->selectJudge(Section::LENGTH);
-    Length->setStartLength();
-    Length->setFinLength(40);
-
-    addSection(sc);
-
-    sc = new Section();
-
-    VirtualLineTracer2* Virtual2 = (VirtualLineTracer2*)sc->selectWalker(Section::VIRTUAL2);
-  
-    Virtual2->setAbsTurnAngle(-45);
-    Virtual2->setParam(10,5, 0.2, 0.2,1,1);
-    LengthJudge* Length = (LengthJudge*)sc->selectJudge(Section::LENGTH);
-    Length->setStartLength();
-    Length->setFinLength(40);
-
-    addSection(sc);
-*/
-
-  // Section* sc = new Section();
-  // addSection(sc);
-
-  /* for(int num=0;p[num].flag==-1;num++){
-        Section* sc = new Section();
-        SimpleWalker* Walker = (SimpleWalker*)sc->selectWalker(p[num].walk);
-
-
-
-        addSection(sc);
-    }*/
-  /* Section* sc = new Section();
-    SimpleWalker* Walker = (SimpleWalker*)sc->selectWalker(Section::WALKER);
-    Walker->setCommand(0,10);
-    TurnAngleJudge* TurnAngle = (TurnAngleJudge*)sc->selectJudge(Section::TURNANGLE);
-    TurnAngle->setStartAngle();
-    TurnAngle->setFinishAngle(-90);
-    addSection(sc);
-*/
-
-  /* Section* sc = new Section();
-
-    VirtualLineTracer2* Virtual2 = (VirtualLineTracer2*)sc->selectWalker(Section::VIRTUAL2);
-  
-    Virtual2->setAbsTurnAngle(0);
-    Virtual2->setParam(40,2, 0.2,0.2,1,1);
-    LengthJudge* Length = (LengthJudge*)sc->selectJudge(Section::LENGTH);
-    Length->setStartLength();
-    Length->setFinLength(90);
-
-    addSection(sc);*/
-
-  /* sc = new Section();
-
-    VirtualLineTracer2* Virtual2 = (VirtualLineTracer2*)sc->selectWalker(Section::VIRTUAL2);
-  
-    Virtual2->setAbsTurnAngle(-45);
-    Virtual2->setParam(10,5, 0.2, 0.2,1,1);
-    LengthJudge* Length = (LengthJudge*)sc->selectJudge(Section::LENGTH);
-    Length->setStartLength();
-    Length->setFinLength(40);
-
-    addSection(sc);
-*/
-
-  //LineTracer(ショートカットなし)
-  /*  LineTracer* tracer = (LineTracer*)sc->selectWalker(Section::TRACER);
-    tracer->setParam(30, 0 ,  30, 0.2, 0.1 );
-    tracer->setEdgeMode(_EDGE);
-    LengthJudge* Length = (LengthJudge*)sc->selectJudge(Section::LENGTH);
-    Length->setStartLength();
-    Length->setFinLength(110);
-     addSection(sc);
-*/
-  //  sc = new Section();
-
-  /*    Section* sc = new Section();
-    VirtualLineTracer* Virtual = (VirtualLineTracer*)sc->selectWalker(Section::VIRTUAL);
-
-
-    Virtual->setRound(30);
-    Virtual->setParam(10,10, 0, 0,1,1);
-    LengthJudge* Length = (LengthJudge*)sc->selectJudge(Section::LENGTH);
-    Length->setStartLength();
-    Length->setFinLength(100);
-
-
-
-    addSection(sc);
-
-    sc = new Section();
-    Virtual = (VirtualLineTracer*)sc->selectWalker(Section::VIRTUAL);
-
-    Virtual->setRound(-30);
-    Virtual->setParam(10,10, 0, 0,1,1);
-    Length = (LengthJudge*)sc->selectJudge(Section::LENGTH);
-    Length->setStartLength();
-    Length->setFinLength(200);
-
-
-
-    addSection(sc);
-*/
-
-  /*sc = new Section();
-    tracer = (LineTracer*)sc->selectWalker(Section::TRACER);
-    tracer->setParam(20, 0 ,  30, 0.2, 0.1 );
-    tracer->setEdgeMode(_EDGE);
-     Length = (LengthJudge*)sc->selectJudge(Section::LENGTH);
-    Length->setStartLength();
-    Length->setFinLength(100);
-    addSection(sc);
-
-*/
-
-  //Section *sc = new Section();
-
-  // LineTracer テスト
-  /*  LineTracer* tracer = (LineTracer*)sc->selectWalker(Section::TRACER);
-
-    tracer->setParam(25, 0 ,  30, 0.2, 0.1 );
-    tracer->setEdgeMode(_EDGE);
-
-    LengthJudge* Length = (LengthJudge*)sc->selectJudge(Section::LENGTH);
-    Length->setFinLength(150);
-
-     addSection(sc);
-    sc = new Section();
-
-
-  SimpleWalker* Walker = (SimpleWalker*)sc->selectWalker(Section::WALKER);
-
-    Walker->run();
-    Walker->setCommand(10,0);
-
-
-    Length = (LengthJudge*)sc->selectJudge(Section::LENGTH);
-    Length->setFinLength(100);
-    */
-  // VirtualLineTracer* Virtual = (VirtualLineTracer*)sc->selectWalker(Section::VIRTUAL);
-
-  //Virtual->setCenterPosition(0,10);
-  //Virtual->setRound(-10);
-  //Virtual->setParam(50,2, 0.2, 0,1,1);
-  //Virtual->run();
-
-  // VirtualLineTracer2* Virtual2 = (VirtualLineTracer2*)sc->selectWalker(Section::VIRTUAL2);
-
-  // Virtual2->setsPosition(0,0);
-  // Virtual2->setfPosition(1000,0);
-  // Virtual2->setAbsTurnAngle(30);
-
-  //Virtual2->setParam(20,1, 0.2, 0.2,1,1);
-
-  //LengthJudge* Length = (LengthJudge*)sc->selectJudge(Section::LENGTH);
-  //Length->setFinLength(1500);
-
-  //TurnAngleJudge* TurnAngle = (TurnAngleJudge*)sc->selectJudge(Section::TURNANGLE);
-  //TurnAngle->setFinishAngle(90);
-
-  //BrightnessJudge* Brightness = (BrightnessJudge*)sc->selectJudge(Section::BRIGHTNESS);
-  //Brightness->setBrightness(0,0);
-
-  //ColorJudge* Color = (ColorJudge*)sc->selectJudge(Section::COLOR);
-  //Color->setColor(140,0);
-
-  /*
-    // SimpleWalkerテスト
-    SimpleWalker* walker = (SimpleWalker*)sc->selectWalker(Section::WALKER);
-    walker->setCommand(10,10);
-    */
 }
 
 void SpeedSectionManager::setWalker(Section *sc)
@@ -242,30 +67,30 @@ void SpeedSectionManager::setJudge(Section *sc)
   {
   case Section::TURNANGLE:
 
-    if (jp[n].lflag == 1)
+    if (wp[n].jflag == 1)
     {
       ((TurnAngleJudge *)judge)->init();
     }
-    ((TurnAngleJudge *)judge)->setFinishAngle(jp[n].fangle);
+    ((TurnAngleJudge *)judge)->setFinishAngle(wp[n].fangle);
 
     break;
   case Section::LENGTH:
 
-    if (jp[n].lflag == 1)
+    if (wp[n].jflag == 1)
     {
       ((LengthJudge *)judge)->init();
     }
-    ((LengthJudge *)judge)->setFinLength(jp[n].flength);
+    ((LengthJudge *)judge)->setFinLength(wp[n].flength);
 
     break;
   case Section::BRIGHTNESS:
 
-    ((BrightnessJudge *)judge)->setBrightness(jp[n].bright1, jp[n].bright2);
+    ((BrightnessJudge *)judge)->setBrightness(wp[n].bright1, wp[n].bright2);
 
     break;
   case Section::COLOR:
 
-    ((ColorJudge *)judge)->setColor(jp[n].color1, jp[n].color2);
+    ((ColorJudge *)judge)->setColor(wp[n].color1, wp[n].color2);
 
     break;
   }
