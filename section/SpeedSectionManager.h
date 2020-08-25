d139c861a8b1c8391c397dbf1c3677e09ecfd801
scvxl11344@yahoo.co.jp <scvxl11344@yahoo.co.jp>
2020-08-25 10:57:18 +0900

    使い方表記

--- section/SpeedSectionManager.h
+++ section/SpeedSectionManager.h
@@ -48,9 +48,20 @@ public:
 protected:
 private:
     int n;
+
+    //{-1の時終了, Section::使いたいwalker, Section::使いたいjudge, 速度, 0, pの値, dの値, iの値, 0, 0 ,
+    ///*setparam*/, 直線仮想ライントレースの角度, 円仮想ライントレースの半径, 単純走行のパワー, 単純走法の曲がり具合, 線のどちらを進むか}
+    //setparamの例,Virtual2->setParam(10,5, 0.2, 0.2,1,1)
+    // tracer->setParam(30,0,30,0.2,0.1)
+    //Virtual->setParam(20,2,0.2,0,1,1)
+    //Walker->setCommand(0,10)
     wParam wp[100] = {{0, Section::TRACER, Section::LENGTH, 20, 0, 30, 0.2, 0.1, 0, 0 /*setparam*/, 0, 0, 0, 0, true},
                       {-1, Section::WNONE, Section::JNONE, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 0, 0, true}};
-    jParam jp[100] = {{1, 1, 0, 200, 0, 0, 0, 0}};
+
+    //{1の時現在地を更新可能, 1の時現在角度を更新可能, 終了角度, 終了距離,白黒割合 , 白黒計測フラグ, 色数値, 彩度数値}
+
+    jParam jp[100] = {{1, 1, 0, 110, 0, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0, 0}};
     void setWalker(Section *sc);
     void setJudge(Section *sc);
 };
