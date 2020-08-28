adf5174ee7541a344142cf36692ea01a6bd23f94
Masayuki HIKICHI <hikichi@nkhs.ac.jp>
2020-08-28 11:52:21 +0900

    パラメータの微調整

--- section/SlalomSectionManager.h
+++ section/SlalomSectionManager.h
@@ -51,7 +51,7 @@ class SlalomSectionManager : public SectionManager
                         {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 5, 0, true,Judge::UPDATE, 0, 32, 0, 0, 0, 0},
                         {0, Section::TAIL, Section::TAILANGLE, 0, 600, 0.5, 2.0, 0.04, 0, 0 /*setparam*/, 0, 0, 0, 0, true,Judge::UPDATE, 10, 0, 0, 0, 0, 0}, // 600度目標で回すが、走行に制御を戻さないといけないので10度で終了
                         {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 8, 0, true,Judge::UPDATE, 0, 8, 0, 0, 0, 0}, //昇段
-                        {0, Section::TAIL, Section::TAILANGLE, 0, 0, 0.0, 0.0, 0.00, 0, 0 /*setparam*/, 0, 0, 0, 0, true,Judge::UPDATE, 590, 0, 0, 0, 0, 0},// 尻尾をなめらかに本体の自重で落とす。早く走行に制御を戻すため、590度で終了
+                        {0, Section::TAIL, Section::TAILANGLE, 0, 0, 0.01, 0.0, 0.00, 0, 0 /*setparam*/, 0, 0, 0, 0, true,Judge::UPDATE, 590, 0, 0, 0, 0, 0},// 尻尾をなめらかに本体の自重で落とす。早く走行に制御を戻すため、590度で終了
                         {0, Section::WALKER, Section::TAILANGLE, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 1, 0, true,Judge::UPDATE, 100, 0 , 0, 0, 0, 0},// 微速前進しながら、尻尾が落ちるのを待つ
                         {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 8, 0, true,Judge::UPDATE, 0, 15 , 0, 0, 0, 0}, //仕上げの前進
                             {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 0, 0, true,Judge::UPDATE, 0, 10000 , 0, 0, 0, 0},  // stop
