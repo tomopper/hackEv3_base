dd4f4bf2b097fca95affdf7dfa9610f586a5fb55
morimori1205 <68767348+morimori1205@users.noreply.github.com>
2020-09-14 17:50:59 +0900

    a

--- Scene.cpp
+++ Scene.cpp
@@ -2,8 +2,7 @@
 
 #include "ev3api.h"
 
-Scene::Scene():
-    mState(UNDEFINED)
+Scene::Scene() : mState(UNDEFINED)
 {
     mSsm = new SpeedSectionManager();
     mBsm = new BingoSectionManager();
@@ -11,30 +10,31 @@ Scene::Scene():
 
 bool Scene::run()
 {
-    switch(mState) {
-        case UNDEFINED:
-            execUndefined();
-            break;
-        case START:
-            execStart();
-            break;
-        case SPEED:
-            execSpeed();
-            break;
-        case INIT_SPEED:
-            initSpeed();
-            break;
-        case BINGO:
-            execBingo();
-            break;
-        case INIT_BINGO:
-            initBingo();
-            break;
-        case GARAGE:
-            execGarage();
-            break;
-        default:
-            return true;
+    switch (mState)
+    {
+    case UNDEFINED:
+        execUndefined();
+        break;
+    case START:
+        execStart();
+        break;
+    case SPEED:
+        execSpeed();
+        break;
+    case INIT_SPEED:
+        initSpeed();
+        break;
+    case SLALOM:
+        execBingo();
+        break;
+    case INIT_SLALOM:
+        initBingo();
+        break;
+    case GARAGE:
+        execGarage();
+        break;
+    default:
+        return true;
     }
     return false;
 }
@@ -43,47 +43,46 @@ void Scene::execUndefined()
 {
     msg_log("Press Touch Button to start.");
     ev3_sensor_config(EV3_PORT_1, TOUCH_SENSOR);
-    mState=START;
+    mState = START;
 }
 void Scene::execStart()
 {
     // とりあえず動かすだけなので、設計に基づいて書き直そう
     if (ev3_touch_sensor_is_pressed(EV3_PORT_1) == 1)
     {
-            mState=INIT_BINGO;
+        mState = INIT_BINGO;
     }
 }
 void Scene::execSpeed()
 {
-    if(mSsm->run()) {
+    if (mSsm->run())
+    {
         delete mSsm;
-         msg_log("test length");
+        msg_log("test length");
         mState = END;
     }
 }
-void Scene::initSpeed(){
+void Scene::initSpeed()
+{
     mSsm->init();
-     mState = SPEED;
-
-
+    mState = SPEED;
 }
 
 void Scene::execBingo()
 {
-    if(mBsm->run()) {
+    if (mBsm->run())
+    {
         delete mBsm;
-         msg_log("test length2");
+        msg_log("test length2");
         mState = END;
     }
-
 }
 
-void Scene::initBingo(){
+void Scene::initBingo()
+{
     mBsm->init();
     mState = BINGO;
-
 }
 void Scene::execGarage()
 {
-
 }
--- section/SpeedSectionManager.h
+++ section/SpeedSectionManager.h
@@ -2,9 +2,6 @@
 #define _SPEED_SECTION_MANAGER_H_
 #include "SectionManager.h"
 
-
-    
-
 typedef struct _Param1
 {
 
@@ -27,7 +24,6 @@ typedef struct _Param1
 
     bool _EDGE; //true=left,false=right
 
-
     Judge::JUDGE_MODE jflag;
 
     float fangle;
@@ -41,14 +37,11 @@ typedef struct _Param1
 
 } wParam;
 
-
-
 class SpeedSectionManager : public SectionManager
 {
 public:
     SpeedSectionManager();
     void init();
-
 
 protected:
 private:
@@ -57,41 +50,26 @@ private:
     //{-1の時終了, Section::使いたいwalker, Section::使いたいjudge, 速度, 0, pの値, dの値, iの値, 0, 0 ,
     ///*setparam*/, 直線仮想ライントレースの角度, 円仮想ライントレースの半径, 単純走行のパワー, 単純走法の曲がり具合, 線のどちらを進むか,
     //1の時現在地を更新可能, 終了角度, 終了距離,白黒割合 , 白黒計測フラグ, 色数値, 彩度数値,止まる秒数}
-    
-    
+
     //setparamの例,Virtual2->setParam(10,5, 0.2, 0.2,1,1)
     // tracer->setParam(30,0,30,0.2,0.1)
     //Virtual->setParam(20,2,0.2,0,1,1)
     //Walker->setCommand(0,10)
 
-   wParam a[100] = { 
-    
-                        {0, Section::TRACER, Section::COLOR, 20, 0, 30, 0.2, 0.1, 0, 0 /*setparam*/, 0, 0, 0, 30, false,Judge::UPDATE, -90, 60, 0, 0, 58, 5,10,false},
-                        {0, Section::VIRTUAL2, Section::LENGTH, -15, 0, 2, 0, 0, 0, 0 /*setparam*/, 180, 0, 0, -30, true,Judge::UNUPDATE, -40, 8, 0, 0, 0, 0,0,false},
-                        {0, Section::WALKER, Section::TURNANGLE, -15, 0,2, 0, 0, 0,0 /*setparam*/, 180, 0, 0, 20, true,Judge::UNUPDATE, -70, 50, 0, 0, 0, 0,0,false},
-                        {0, Section::WALKER, Section::TURNANGLE, -15, 0,2, 0, 0, 0,0 /*setparam*/, 180, 0, 0, 5, true,Judge::UNUPDATE, -90, 50, 0, 0, 0, 0,0,false},
-                        {0, Section::WALKER, Section::STOP, -15, 0, 2, 0, 0, 0, 0 /*setparam*/, -85, 0, 0, 0, true,Judge::UPDATE, -90, 50, 0, 0, 0, 0,50,false},
-                        {0, Section::VIRTUAL2, Section::LENGTH, 15, 0, 20, 0, 0, 0, 0 /*setparam*/, -90, 0, 0, -30, true,Judge::UNUPDATE, -30, 35, 0, 0, 0, 0,0,false},
-                        {0, Section::TRACER, Section::LENGTH, 10, 0, 30, 0.2, 0.1, 0, 0 /*setparam*/, 0, 0, 0, 30, true,Judge::UNUPDATE, -90, 55, 0, 0, 58, 5,10,false},
-                        {0, Section::VIRTUAL2, Section::LENGTH, 15, 0, 20, 0, 0, 0, 0 /*setparam*/, -90, 0, 0, -30, true,Judge::UNUPDATE, -30, 75, 0, 0, 0, 0,0,false},
-                        {0, Section::TRACER, Section::LENGTH, 10, 0, 30, 0.2, 0.1, 0, 0 /*setparam*/, 0, 0, 0, 30, true,Judge::UNUPDATE, -90, 90, 0, 0, 58, 5,10,false},
-                        {0, Section::VIRTUAL2, Section::LENGTH, 15, 0, 20, 0, 0, 0, 0 /*setparam*/, -90, 0, 0, -30, true,Judge::UNUPDATE, -30, 120, 0, 0, 0, 0,0,false},
-                        {0, Section::WALKER, Section::TURNANGLE, -15, 0,2, 0, 0, 0,0 /*setparam*/, 180, 0, 0, -30, true,Judge::UNUPDATE, -10, 50, 0, 0, 0, 0,0,false},
-                        {0, Section::WALKER, Section::TURNANGLE, -15, 0,2, 0, 0, 0,0 /*setparam*/, 180, 0, 0, -10, true,Judge::UNUPDATE, 0, 50, 0, 0, 0, 0,0,false},
-                        {0, Section::WALKER, Section::STOP, -15, 0, 2, 0, 0, 0, 0 /*setparam*/, -85, 0, 0, 0, true,Judge::UPDATE, -90, 50, 0, 0, 0, 0,50,false},
-                        {0, Section::VIRTUAL2, Section::LENGTH, 15, 0, 2, 0, 0, 0, 0 /*setparam*/, 0, 0, 0, -30, true,Judge::UNUPDATE, -40, 180, 0, 0, 0, 0,0,false},
-                        {0, Section::VIRTUAL2, Section::LENGTH, -15, 0, 2, 0, 0, 0, 0 /*setparam*/, 180, 0, 0, -30, true,Judge::UPDATE, -40,-20 , 0, 0, 0, 0,0,false},
-                        {0, Section::WALKER, Section::STOP, -15, 0, 2, 0, 0, 0, 0 /*setparam*/, -85, 0, 0, 0, true,Judge::UNUPDATE, -90, 50, 0, 0, 0, 0,50,false},
-                        {0, Section::VIRTUAL2, Section::LENGTH, 15, 0, 20, 0, 0, 0, 0 /*setparam*/, -90, 0, 0, -30, true,Judge::UNUPDATE, -30, 20, 0, 0, 0, 0,0,false},
-                        {-1, Section::WNONE, Section::JNONE, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 0, 0, true, Judge::UNUPDATE, 0, 0, 0, 0, 0, 0,0,false}};
-
-wParam *array[10]={a,a,a};
-  wParam *wp;
-
+    wParam wp[100] = {{0, Section::VIRTUAL2, Section::LENGTH, 20, 0, 30, 0.2, 0.1, 0, 0 /*setparam*/, 0, 0, 30, 0, false, Judge::UPDATE, 0, 57.8, 0, 0, 0, 0},
+                      {0, Section::VIRTUAL, Section::LENGTH, 20, 0, 30, 0.2, 0.1, 0, 0 /*setparam*/, 0, 65, 0, 30, false, Judge::UPDATE, 0, 105, 0, 0, 0, 0},
+                      {0, Section::TRACER, Section::LENGTH, 20, 0, 30, 0.2, 0.1, 0, 0 /*setparam*/, 0, 0, 0, 30, true, Judge::UPDATE, 0, 100, 0, 0, 0, 0},
+                      {0, Section::TRACER, Section::LENGTH, 10, 0, 30, 0.2, 0.1, 0, 0 /*setparam*/, 0, 0, 0, 30, true, Judge::UPDATE, 0, 13, 0, 0, 0, 0},
+                      {0, Section::VIRTUAL2, Section::LENGTH, -5, 0, 0, 0, 0, 0, 0 /*setparam*/, 180, 0, 30, 0, true, Judge::UPDATE, 0, -5, 0, 0, 0, 0},
+                      {0, Section::VIRTUAL2, Section::LENGTH, -20, 0, 0, 0, 0, 0, 0 /*setparam*/, 180, 0, 30, 0, false, Judge::UPDATE, 0, -120, 0, 0, 0, 0},
+                      //{0, Section::WALKER, Section::LENGTH, 20, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, -30, 8, false, Judge::UPDATE, 0, -45, 0, 0, 0, 0},
+                      {0, Section::VIRTUAL, Section::TURNANGLE, -20, 0, 6, 0, 0, 0, 0 /*setparam*/, 0, -100, 0, 0, false, Judge::UPDATE, -90, 0, 0, 0, 0, 0},
+                      {0, Section::VIRTUAL2, Section::LENGTH, -20, 0, 2, 0, 0, 0, 0 /*setparam*/, 180, 0, 30, 0, false, Judge::UPDATE, 0, -30, 0, 0, 0, 0},
+                      {0, Section::TRACER, Section::LENGTH, 20, 0, 30, 0.2, 0.1, 0, 0 /*setparam*/, 0, 0, 0, 30, false, Judge::UPDATE, 0, 200, 0, 0, 0, 0},
+                      {0, Section::WALKER, Section::STOP, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 0, 0, true, Judge::UNUPDATE, 0, 0, 0, 0, 0, 0, 0},
+                      {-1, Section::WNONE, Section::JNONE, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 0, 0, true, Judge::UNUPDATE, 0, 0, 0, 0, 0, 0}};
 
     void setWalker(Section *sc);
     void setJudge(Section *sc);
 };
 #endif
-
-
