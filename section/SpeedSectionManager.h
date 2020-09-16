f441c78641ac3cdc93d534cbb86c58fdc5294673
morimori1205 <68767348+morimori1205@users.noreply.github.com>
2020-09-16 18:12:09 +0900

    あ

--- section/SpeedSectionManager.h
+++ section/SpeedSectionManager.h
@@ -67,7 +67,7 @@ private:
                    {0, Section::TRACER, Section::LENGTH, 15, 0, 30, 0.3, 0.2, 0, 0 /*setparam*/, 0, 0, 0, 30, true, Judge::UPDATE, 0, 8, 0, 0, 0, 0, 0},
                    {0, Section::TRACER, Section::LENGTH, 25, 0, 20, 0.3, 0.2, 0, 0 /*setparam*/, 0, 0, 0, 30, true, Judge::UNUPDATE, 0, 15, 0, 0, 0, 0, 0},
                    {0, Section::TRACER, Section::LENGTH, 35, 0, 30, 0.2, 0.1, 0, 0 /*setparam*/, 0, 0, 0, 30, true, Judge::UPDATE, 0, 25, 0, 0, 0, 0, 0},
-                   {0, Section::TRACER, Section::LENGTH, 25, 0, 30, 0.2, 0.2, 0, 0 /*setparam*/, 0, 0, 0, 30, true, Judge::UNUPDATE, 0, 100, 0, 0, 0, 0, 0},
+                   {0, Section::TRACER, Section::LENGTH, 25, 0, 30, 0.2, 0.2, 0, 0 /*setparam*/, 0, 0, 0, 30, true, Judge::UNUPDATE, 0, 90, 0, 0, 0, 0, 0},
                    {0, Section::VIRTUAL2, Section::LENGTH, 20, 0, 0.1, 0, 0, 0, 0 /*setparam*/, 0, 0, 30, 0, false, Judge::UPDATE, 0, 2, 0, 0, 0, 0, 0},
                    {0, Section::VIRTUAL2, Section::LENGTH, 10, 0, 0.1, 0, 0, 0, 0 /*setparam*/, 0, 0, 30, 0, false, Judge::UNUPDATE, 0, 4, 0, 0, 0, 0, 0},
                    //{0, Section::ARM, Section::ARMANGLE, 0, -70, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 0, 0, true, Judge::UPDATE, -69, 0, 0, 0, 0, 0, 0},
