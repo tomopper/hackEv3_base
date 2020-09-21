ce67ffb43e65e4399a5903d533ede0c2ca68aabb
smurata297 <sayatano@outlook.jp>
2020-09-21 15:33:55 +0900

    粗めの調整

--- section/SlalomSectionManager.h
+++ section/SlalomSectionManager.h
@@ -54,28 +54,31 @@ class SlalomSectionManager : public SectionManager
         
         int n;
         wsParam a[100] = {
-                        // {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 50, 0, true,Judge::UPDATE, 0, 37, 0, 0, 0, 0, 0}, //尻尾,アーム不使用
+                        // {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 40, 0, true,Judge::UPDATE, 0, 37, 0, 0, 0, 0, 0}, //尻尾,アーム不使用
                         {0, Section::ARM, Section::ARMANGLE, 0, -58, 1.5, 0, 0, 0, 0/*setparam*/, 0, 0, 0, 0, true,Judge::UPDATE, -58, 0, 0, 0, 0, 0, 0},
                         {0, Section::TRACER, Section::LENGTH, 5, 0, 1, 0.2, 0.1, 1, 1 /*setparam*/, 0, 0, 0, 0, true,Judge::UPDATE, 0, 20, 0, 0, 0, 0, 0},
                         {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, -5, 0, true,Judge::UPDATE, 0, -0.5, 0, 0, 0, 0, 0},
                         {0, Section::ARM, Section::ARMANGLE, 0, 30, 1, 0, 0, 0, 0/*setparam*/, 0, 0, 0, 0, true,Judge::UPDATE, 9.5, 0, 0, 0, 0, 0, 0},
-                        {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 12, 0, true,Judge::UPDATE, 0, 22, 0, 0, 0, 0, 0},
+                        {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 15, 0, true,Judge::UPDATE, 0, 22, 0, 0, 0, 0, 0},
                         {0, Section::TAIL, Section::TAILANGLE, 0, 600, 0.6, 0.4, 0.04, 0, 0 /*setparam*/, 0, 0, 0, 0, true,Judge::UPDATE, 360, 0, 0, 0, 0, 0, 0}, // 600度目標で回すが、走行に制御を戻さないといけないので10度で終了
                         {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 8, 0, true,Judge::UPDATE, 0, 2, 0, 0, 0, 0, 0}, //昇段
                         {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 0.5, 0, true,Judge::UPDATE, 0, 0.1, 0, 0, 0, 0, 0},
                         {0, Section::TAIL, Section::TAILANGLE, 0, 0, 1, 0.0, 0.00, 0, 0 /*setparam*/, 0, 0, 0, 0, true,Judge::UPDATE, 6, 0, 0, 0, 0, 0, 0},// 尻尾をなめらかに本体の自重で落とす。早く走行に制御を戻すため、590度で終了
                         {0, Section::ARM, Section::ARMANGLE, 0, -50, 2, 0, 0, 0, 0/*setparam*/, 0, 0, 0, 0, true,Judge::UPDATE, -50, 0, 0, 0, 0, 0, 0},
                         {0, Section::TRACER, Section::LENGTH, 5, -0.7, 30, 0.2, 0.1, 1, 1 /*setparam*/, 0, 0, 0, -30, false,Judge::UPDATE, 0, 10, 0, 0, 0, 0, 0},
-                        {0, Section::VIRTUAL, Section::LENGTH, 10, 0, 6, 0, 0, 1, 1 /*setparam*/, 0, 40, 0, 0, true,Judge::UPDATE, 0, 17, 0, 0, 0, 0, 0},
-                        {0, Section::VIRTUAL, Section::TURNANGLE, 8, 0, 9, 0, 0, 1.4, 1 /*setparam*/, 0, -27, 0, 0, true,Judge::UPDATE, -60, 33, 0, 0, 0, 0, 0},
+                        {0, Section::VIRTUAL, Section::LENGTH, 8, 0, 6, 0, 0, 1, 1 /*setparam*/, 0, 40, 0, 0, true,Judge::UPDATE, 0, 17, 0, 0, 0, 0, 0},
+                        {0, Section::VIRTUAL, Section::TURNANGLE, 8, 0, 9, 0, 0, 1, 1 /*setparam*/, 0, -27, 0, 0, true,Judge::UPDATE, -60, 33, 0, 0, 0, 0, 0},
                         // {0, Section::VIRTUAL, Section::TURNANGLE, 8, 0, 30, 0, 0, 0.6, 1 /*setparam*/, 0, 30, 0, 0, true,Judge::UPDATE, 18, 8.5, 0, 0, 0, 0, 0},
-                        {0, Section::VIRTUAL, Section::TURNANGLE, 4, 0, 30, 0, 0, 1, 1 /*setparam*/, 0, 18, 0, 0, true,Judge::UPDATE, 61, 22.5, 0, 0, 0, 0, 0},
+                        {0, Section::VIRTUAL, Section::TURNANGLE, 5, 0, 30, 0, 0, 1, 1 /*setparam*/, 0, 18.7, 0, 0, true,Judge::UPDATE, 65.8, 22.5, 0, 0, 0, 0, 0},
                         // {0, Section::VIRTUAL, Section::TURNANGLE, 8, 0, 5, 0, 0, 1.4, 1 /*setparam*/, 0, -14, 0, 0, true,Judge::UPDATE, -13, 16, 0, 0, 0, 0, 0},
-                        {0, Section::VIRTUAL, Section::TURNANGLE, 4, 0, 5, 0, 0, 1, 1 /*setparam*/, 0, -18, 0, 0, true,Judge::UPDATE, -60, 20.3, 0, 0, 0, 0, 0},
-                        {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 5, 0.4, true,Judge::UPDATE, 0, 17, 0, 0, 0, 0, 0},
-                        {0, Section::WALKER, Section::TURNANGLE, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 0, 3, true,Judge::UPDATE, -50, 0, 0, 0, 0, 0, 0},
-                        {0, Section::TRACER, Section::LENGTH, 5, -0.7, 30, 0.2, 0.1, 1, 1 /*setparam*/, 0, 0, 0, -30, false,Judge::UPDATE, 0, 10, 0, 0, 0, 0, 0},
-                        {0, Section::TRACER, Section::LENGTH, 15, -0.7, 30, 0.2, 0.1, 1, 1 /*setparam*/, 0, 0, 0, 30, false,Judge::UPDATE, 0, 40, 0, 0, 0, 0, 0},
+                        {0, Section::VIRTUAL, Section::TURNANGLE, 5, 0, 5, 0, 0, 1.1, 1 /*setparam*/, 0, -19.8, 0, 0, true,Judge::UPDATE, -81.5, 20.3, 0, 0, 0, 0, 0},
+                        {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 9, 0.7, true,Judge::UPDATE, 0, 11.27, 0, 0, 0, 0, 0},
+                        {0, Section::VIRTUAL, Section::TURNANGLE, 5, 0, 5, 0, 0, 1, 1 /*setparam*/, 0, 30, 0, 0, true,Judge::UPDATE, 10.5, 22.5, 0, 0, 0, 0, 0},
+                        // {0, Section::VIRTUAL, Section::TURNANGLE, 4, 0, 5, 0, 0, 1, 1 /*setparam*/, 0, -10, 0, 0, true,Judge::UPDATE, -70, 20.3, 0, 0, 0, 0, 0},
+                        // {0, Section::WALKER, Section::LENGTH, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 5, 0.4, true,Judge::UPDATE, 0, 17, 0, 0, 0, 0, 0},
+                        {0, Section::WALKER, Section::TURNANGLE, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 0, 10, true,Judge::UPDATE, -40, 0, 0, 0, 0, 0, 0},
+                        {0, Section::TRACER, Section::LENGTH, 5, -0.4, 30, 0.2, 0.1, 1, 1 /*setparam*/, 0, 0, 0, -30, false,Judge::UPDATE, 0, 10, 0, 0, 0, 0, 0},
+                        {0, Section::WALKER, Section::LENGTH, 15, -0.4, 30, 0.2, 0.1, 1, 1 /*setparam*/, 0, 0, 40, 0, false,Judge::UNUPDATE, 0, 42, 0, 0, 0, 0, 0},
                         {0, Section::WALKER, Section::STOP, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 0, 0, true,Judge::UPDATE, 0, 0, 0, 0, 0, 0, 10},
                         {-1, Section::WNONE, Section::JNONE, 0, 0, 0, 0, 0, 0, 0 /*setparam*/, 0, 0, 0, 0, true, Judge::UNUPDATE, 0, 0, 0, 0, 0, 0, 0}};
 
